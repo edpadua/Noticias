dd858953bef48759f446b7e49f33617ec0902104
edpadua <ed.padua@gmail.com>
2023-04-22 10:09:54 -0300

    Primeiros componentes e contexto
    Primeiros componentes e contexto

---
+++ noticias/src/Componentes/Cabecalho/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './Cabecalho.module.css';
+
+function Cabecalho() {
+  return (
+    <header className={styles.cabecalho}>
+            
+            <nav>
+                
+            </nav>
+    </header>
+  )
+}
+
+export default Cabecalho
